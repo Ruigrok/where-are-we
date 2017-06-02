c588a5c50d1be1ba6d9021446c017739e2d5f593
Aelly Liu <laellyl@gmail.com>
2017-06-01 20:48:36 -0500

    finished the function to display photos in a carousel

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -229,7 +229,69 @@ function callback(results, status) {
     }
   }
 }
+//function for display target destination photos
+function displayPlacePhotos()
+  {
+    //tmp array for testing purpose
+    var photoRefIds=["https://lh6.googleusercontent.com/-IoCBf7ZHrCs/V4dZSvupvzI/AAAAAAAA0mQ/YxzSoz0FiKg-kUdmimLSPk3sG-Bs0f5qACJkC/w400-h400-k/","https://lh3.googleusercontent.com/-C4-0ZpPCcLM/WE74Ae8DFYI/AAAAAAAB1Jw/8qF8gy2h5ioaMtnJxMZs61yRMJ_HLW81ACLIB/w400-h400-k/"];
 
+    //this is setting up the phot carousel structure
+    var carouselId="photoCarousel";
+    var carouselRow=$("<div>");
+    carouselRow.addClass("row");
+    var carouselCol=$("<div>");
+    carouselCol.addClass("col-md-8 col-md-offset-3");
+    var carouselContent=$("<div>");
+    carouselContent.addClass("carousel slide");
+    carouselContent.attr("id",carouselId);
+    carouselContent.attr("data-ride","carousel");
+    var carIndicators=$("<ol>");
+    carIndicators.addClass("carousel-indicators");
+    var carInner=$("<div>");
+    carInner.addClass("carousel-inner");
+    carInner.attr("role","listbox");
+
+    for(var i =0; i< photoRefIds.length;i++)
+    {
+      //list tags for carousel indicators section
+      var carList=$("<li>");
+      carList.attr("data-target",carouselId);
+      carList.attr("data-slide-to",i);
+
+      //add items to carousel inner section
+      var carItem=$("<div>");
+      
+      var imgItem=$("<img>");
+      imgItem.attr("src",photoRefIds[i]);
+     
+      if(i===0)
+      {
+        carList.addClass("active");
+        carItem.addClass("item active");
+      }
+      else
+      {
+        carList.addClass("");
+        carItem.addClass("item");
+      }
+      carIndicators.append(carList);
+
+      carItem.append(imgItem);
+      carInner.append(carItem);
+    }
+
+    carouselContent.append(carIndicators);
+    carouselContent.append(carInner);
+    carouselCol.append(carouselContent);
+    //adding left and right arrow
+    carouselContent.append("<a class=\"left carousel-control\" href=\"#photoCarousel\" role=\"button\" data-slide=\"prev\"><span class=\"glyphicon glyphicon-chevron-left\" aria-hidden=\"true\"></span><span class=\"sr-only\">Previous</span></a><a class=\"right carousel-control\" href=\"#photoCarousel\" role=\"button\" data-slide=\"next\"><span class=\"glyphicon glyphicon-chevron-right\" aria-hidden=\"true\"></span><span class=\"sr-only\">Next</span></a>")
+    carouselRow.append(carouselCol);
+
+    // return carouselRow;
+    $("#gamePlay").append(carouselRow);
+// var photoDisp=displayPlacePhotos();
+  // $("#gameplay").append(photoDisp);
+}
 
 
 
