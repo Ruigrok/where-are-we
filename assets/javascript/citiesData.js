98bd1348cfe8b1f9a7a85a25d3b6913dd3669ff9
AC Ruigrok <andrew.c.ruigrok@gmail.com>
2017-06-01 21:27:57 -0500

    adding initial cities

---
+++ assets/javascript/citiesData.js
@@ -0,0 +1,43 @@
+var cities = [
+  {
+    name: "New York",
+    lat: 40.7128,
+    lng: -74.0059,
+  },
+  {
+      name: "Buenos Aires",
+      lat: -34.6037,
+      lng: -58.3816
+  },
+    {
+      name: "Bogotá",
+      lat: 4.7110,
+      lng: -74.0721
+  },
+    {
+      name: "Rio de Janeiro",
+      lat: 22.9068,
+      lng: -43.1729,
+  },
+    {
+      name: "Lima",
+      lat: -12.0464,
+      lng: -77.0428,
+  },
+    {
+      name: "Quito",
+      lat: -0.1807,
+      lng: -78.4678
+  },
+    {
+      name: 
+      lat:
+      lng:
+  },
+      
+
+];
+
+
+0.1807° S, 78.4678° W
+
